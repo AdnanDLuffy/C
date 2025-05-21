5843b80c9baf0f1cecd05d8e4dd8b0ba895dd10e
Adnan <103826456+AdnanDLuffy@users.noreply.github.com>
2025-05-21 14:54:30 -0400

    Update myFirst.c

--- myFirst.c
+++ myFirst.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main () {
 int age = 18;
+int x,y,z;
+  x=y=z = 50 ;
 printf("Assalamualaikum \n I am Adnan \n");
 printf("And I am %d year old year",age);
   //Hehe
+printf(y);  
   return 0 ;
 }
